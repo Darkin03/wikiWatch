c93ff2cb97e7d306f69aa9c9b93298f89dc7182a
Darkin03 <esnaiderortega@unisinu.edu.co>
2023-03-16 15:34:07 -0500

    Modificando el footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,21 @@
+import { CiFacebook, CiInstagram, CiLinkedin, CiMail, IconName } from "react-icons/ci";
+
 export function WebEnd(){
     return(
         <footer>
             <h1>2023</h1>
-            
+            <div>
+                <div>
+                    The data in this app is from The Movie DB 
+                </div>
+                <div>
+                    <CiInstagram/>
+                    <CiFacebook/>
+                    <CiLinkedin/>
+                    <CiMail/>
+                </div>
+            </div>
+            <p>You can wear the code of this app! </p>
         </footer>
     )
 }
