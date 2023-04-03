ec22989aab221b7832f22307780f8470161c18b9
Esnaider.OR <80131310+Darkin03@users.noreply.github.com>
2023-04-03 14:59:19 -0500

    Update movies.js

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,79 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
-const apiKey = '5cec1a15e2c219c4f08d84958efb00e7';
+const apiKey ='5cec1a15e2c219c4f08d84958efb00e7';
 
-const links = {
+const links = { 
   latest: 'https://api.themoviedb.org/3/movie/latest?language=en-US',
-  popular: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
-  topRate: 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
-  inTheatres: 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
-  upComing: 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1',
-}
+};
 
-async function getLatestMovie(link) {
-  try {
-    const response = await axios.get(link + `&api_key=${apiKey}`);
-    const moviesLatest = {
-      id: response.data.id,
-      adult: response.data.adult,
-      status: response.data.status,
-      title: response.data.original_title,
-      overview: response.data.overview,
-      poster_path: response.data.poster_path,
-      popularity: response.data.popularity,
-      vote_average: response.data.vote_average,
-      vote_count: response.data.vote_average,
-      genres: response.data.genres,
-    }
-    return moviesLatest;
-  } catch (error) {
-    console.error(error);
-  }
-}
+ export async function getLatest() {
+  const response = await axios.get(links.latest, {
+    params: {
+      api_key: apiKey,
+    },
+  });
+  const moviesLatest = {
+    id: response.data.id,
+    adult: response.data.adult,
+    status: response.data.status,
+    title: response.data.title,
+    overview: response.data.overview,
+    poster_path: response.data.poster_path,
+    popularity: response.data.popularity,
+    vote_average: response.data.vote_average,
+    vote_count: response.data.vote_average,
+    genres: response.data.genres,
+  };
 
-async function getMovies(link) {
-  try {
-    const response = await axios.get(link + `&api_key=${apiKey}`);
-    const numberOfMovies = 8;
-    const movies = response.data.results.map((movie) => (
-      {
-        id: movie.id,
-        vote_average: movie.vote_average,
-        poster_path: movie.poster_path
-      }
-    ));
-    return movies;
-  } catch (error) {
-
-  }
-}
-
-async function getSummaryMovies(link) {
-  try {
-    const response = await axios.get(link + `&api_key=${apiKey}`);
-    const numberOfMovies = 8;
-    const movies = response.data.results.map((movie) => (
-      {
-        id: movie.id,
-        vote_average: movie.vote_average,
-        poster_path: movie.poster_path
-      }
-    ));
-
-    return movies.slice(0, numberOfMovies);
-  } catch (error) {
-
-  }
-}
+  return moviesLatest;
+};
 
 
-//exporting all movie summaries 
-export const latestMovie = await getLatestMovie(links.latest);
-export const popularMovies = await getMovies(links.popular);
-export const summaryPopularMovies = await getSummaryMovies(links.popular);
-export const topRatedMovies = await getMovies(links.topRate);
-export const summaryTopRatedMovies = await getSummaryMovies(links.topRate);
-export const inTheatresMovies = await getMovies(links.inTheatres);
-export const summaryInTheatresMovies = await getSummaryMovies(links.inTheatres);
-export const upComingMovies = await getMovies(links.upComing);
-export const summaryUpComingMovies = await getSummaryMovies(links.upComing);
